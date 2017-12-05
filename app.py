70707c70adaf61588f0378713f0522517df9610b
Min Ah Kim <mak877@nyu.edu>
2017-12-05 13:37:10 -0500

    cleaned up app.py

--- app.py
+++ app.py
@@ -177,142 +177,12 @@ def logout():
 	session.pop('username')
 	return redirect('/')
 
-@app.route('/profile') #MUST IMPLEMENT (1. my profile, 2. other profiles)
+@app.route('/profile') #MUST IMPLEMENT NOT MY profiles
 @login_required
 def profile():
     return render_template('profile.html')
 
-@app.route('/notif') #MUST IMPLEMENT
-@login_required
-def notif():
-    return render_template('notif.html')
-
-@app.route('/home_nav') #MAY NEED TO IMPLEMENT, NOT SURE
-def home_nav():
-    return redirect(url_for('home'))
-
-app.secret_key = 'some key that you will never guess'
-
-if __name__ == "__main__":
-		app.run(debug=True)
-
-
-
-from flask import Flask, render_template, request, redirect, session, url_for
-import pymysql.cursors
-from hashlib import sha1
-
-app = Flask(__name__)
-
-#Connect to MadChatter DB
-conn = pymysql.connect(host='localhost',
-	port=8889,
-	user='root',
-	password='root',
-	db='MadChatter',
-	charset='utf8mb4',
-	cursorclass=pymysql.cursors.DictCursor)
-
-@app.route("/")
-def main():
-	return render_template('index.html')
-
-@app.route("/login")
-def login():
-	return render_template('login.html')
-
-@app.route("/register")
-def register():
-	return render_template('register.html')
-
-@app.route("/home")
-def home():
-	username = session['username']
-	cursor = conn.cursor();
-	query = 'SELECT timest,file_path,content_name FROM content WHERE username = %s OR public = 1 ORDER BY timest DESC'
-	cursor.execute(query, (username))
-	data = cursor.fetchall()
-	cursor.close()
-	return render_template('home.html', username=username, posts=data)
-
-#Register new user
-@app.route('/registerAuth', methods=['GET', 'POST'])
-def registerAuth():
-	#pull info from registration page
-	username = request.form['username']
-	password = request.form['password']
-	fname = request.form['first_name']
-	lname = request.form['last_name']
-	email = request.form['email']
-
-	cursor = conn.cursor()
-	#Check for existance of new user
-	query = 'SELECT * FROM person WHERE username = %s'
-	cursor.execute(query, (username))
-	print ("existence check complete")
-	data = cursor.fetchone()
-	err = None
-	if(data):
-		error = "The user "+username+" already exists"
-		return render_template('register.html', error = err)
-	else:
-		ins = 'INSERT INTO person (username,password,first_name,last_name,email) VALUES(%s, sha1(%s), %s, %s, %s)'
-		cursor.execute(ins, (username, password.encode(), fname, lname, email))
-		conn.commit()
-		cursor.close()
-		return render_template('index.html')
-
-#log in user
-@app.route('/loginAuth', methods=['GET', 'POST'])
-def loginAuth():
-	#grabs information from the forms
-
-	username = request.form['username']
-	password = request.form['password']
-
-	#retrieves username and password
-	cursor = conn.cursor()
-	query = 'SELECT password FROM person WHERE username = %s AND password = sha1(%s)'
-	cursor.execute(query, (username, password.encode()))
-	data = cursor.fetchone()
-	cursor.close()
-	error = None
-	if(data):
-		#creates a session for the the user
-		session['username'] = username
-		return redirect(url_for('home'))
-	else:
-		#returns an error message to the html page
-		error = 'Invalid login or username'
-		return render_template('login.html', error=error)
-
-
-@app.route('/resetPassword', methods=['POST'])
-def resetPassword():
-	return redirect('/')
-
-
-@app.route('/post', methods=['GET', 'POST'])
-def post():
-	username = session['username']
-	cursor = conn.cursor();
-	content = request.form['content']
-	description = request.form['description']
-	public = '0'
-	if(request.form.get('makePublic')):
-		public = '1'
-	query = 'INSERT INTO content (username,file_path,content_name,public) VALUES(%s, %s, %s, %s)'
-	cursor.execute(query, (username,content,description,public))
-	conn.commit()
-	cursor.close()
-	return redirect(url_for('home'))
-
-@app.route('/logout')
-def logout():
-	session.pop('username')
-	return redirect('/')
-
-@app.route('/myProfile') #MUST IMPLEMENT (1. my profile, 2. other profiles)
+@app.route('/myProfile')
 def myProfile():
 	username = session['username']
 	cur = conn.cursor();
@@ -327,13 +197,12 @@ def myProfile():
 	return render_template('profile.html', user=username,friends=friendsList)
 
 @app.route('/notif') #MUST IMPLEMENT
+@login_required
 def notif():
     return render_template('notif.html')
 
 app.secret_key = 'some key that you will never guess'
 
 if __name__ == "__main__":
-		app.run()
-
-
+		app.run(debug=True)
 
