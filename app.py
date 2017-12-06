a9f9ee0ebc798bc39caafe8244e31f6bd8bb5842
dchengs <d.chengsw@gmail.com>
2017-12-06 00:43:59 -0500

    New Features
    - reset password and change password are now fully functional
    - any html files associated with the above functions are removed and replaced

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
+#still need a random password generator for password reset
 from flask import Flask, render_template, request, redirect, session, url_for
 import pymysql.cursors
 from hashlib import sha1
+from functools import wraps
+from socket import *
+from datetime import datetime
+import email_module as em
 
 app = Flask(__name__)
 
 #Connect to MadChatter DB
 conn = pymysql.connect(host='localhost',
-	port=8889,
+	port=3306,
 	user='root',
-	password='root',
+	password='',
 	db='MadChatter',
 	charset='utf8mb4',
 	cursorclass=pymysql.cursors.DictCursor)
+
+#wrap around functions that are only available to logged in users
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route("/")
 def main():
@@ -19,17 +34,20 @@ def main():
 
 @app.route("/login")
 def login():
-	return render_template('login.html')
+    #if 'logged_in' in session:
+        #return redirect(url_for('home'))
+    return render_template('login.html')
 
 @app.route("/register")
 def register():
 	return render_template('register.html')
 
-@app.route("/home.html")
+@app.route("/home")
+@login_required
 def home():
 	username = session['username']
 	cursor = conn.cursor();
-	query = 'SELECT timest,file_path,content_name FROM content WHERE username = %s OR public = 1 ORDER BY timest DESC'
+	query = 'SELECT id, timest, file_path, content_name FROM content WHERE username = %s OR public = 1 ORDER BY timest DESC'
 	cursor.execute(query, (username))
 	data = cursor.fetchall()
 	cursor.close()
@@ -61,7 +79,19 @@ def registerAuth():
 		conn.commit()
 		cursor.close()
 		return render_template('index.html')
-
+	    
+#function checks username and password if they match and return boolean value
+def check_user_pw(username, password):
+    cursor = conn.cursor()
+    inst = "SELECT * FROM person WHERE username = %s AND password = sha(%s)"
+    cursor.execute(inst, (username, password))
+    valid = cursor.fetchone()
+    cursor.close()
+    if valid:
+        return True
+    else:
+        return False
+    
 #log in user
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
@@ -71,15 +101,13 @@ def loginAuth():
 	password = request.form['password']
 
 	#retrieves username and password
-	cursor = conn.cursor()
-	query = 'SELECT password FROM person WHERE username = %s AND password = sha1(%s)'
-	cursor.execute(query, (username, password.encode()))
-	data = cursor.fetchone()
-	cursor.close()
-	error = None
-	if(data):
+	boo_value = check_user_pw(username, password)
+	
+	if(boo_value):
 		#creates a session for the the user
+                #session['logged_in'] tracks if user =logged in
 		session['username'] = username
+		session['logged_in'] = True
 		return redirect(url_for('home'))
 	else:
 		#returns an error message to the html page
@@ -87,13 +115,95 @@ def loginAuth():
 		return render_template('login.html', error=error)
 
 
-@app.route('/resetPassword', methods=['POST'])
-def resetPassword():
-	return redirect('/')
 
+@app.route('/change_password', methods = ['GET','POST'])
+def change_password():
+    if request.method == 'POST':
+        username = session['username']
+        current_password = request.form['current_password']
+        new_password = request.form['new_password']
+        confirm_password = request.form['confirm_password']
+        if (check_user_pw(username, current_password) and (new_password == confirm_password)):
+            cursor = conn.cursor()
+            inst = ("UPDATE person SET password = sha1(%s) WHERE username = %s")
+            cursor.execute(inst, (new_password, username))
+            conn.commit()
+            cursor.close()
+            return redirect(url_for('home'))
+        elif (new_password != confirm_password):
+            ##need to implement error message
+            return rediret(url_for('login'))
+        else:
+            return rediret(url_for('login'))
+    return render_template('change_password.html')
 
+            
+@app.route('/forgot_password')
+def forgot_password():
+    return render_template('reset_password.html')
+
+@app.route('/reset_password', methods = ['GET', 'POST'])
+def reset_password():
+    username = request.form['username']
+    email = request.form['email']
+    checkAccount(username, email)
+    return redirect(url_for('login'))
+
+#when user resets password, function checks for associated account and mail password
+def checkAccount(username, email):
+        inst = 'SELECT * FROM person WHERE username = %s AND email = %s'
+        cursor = conn.cursor()
+        cursor.execute(inst, (username, email))
+        data = cursor.fetchone()
+        cursor.close()
+        #if account exists, send email
+        if data:
+                email = data['email']
+                new_pw = '123123123'
+                inst = 'UPDATE person SET password = sha1(%s) WHERE username = %s AND email = %s'
+                cursor = conn.cursor()
+                cursor.execute(inst, (new_pw, username, email))
+                conn.commit()
+                cursor.close()
+                em.mailPassword(new_pw, email)      
+        #if account doesn't exist, return 0
+        else:
+                return 'failed'
+            
+#function determines if target username exists            
+def search_user(target_username):
+    cursor = conn.cursor()
+    inst = "SELECT * FROM person WHERE username = %s"
+    cursor.execute(inst, (username))
+    exist = cursor.fetchone()
+    cursor.close()
+    if exist:
+        return True
+    else:
+        return False
+    
+@app.route('/add_friend/<group_name>', methods = ['GET', 'POST'])
+def add_friend(group_name):
+    if request.method == 'POST':
+        target_username = request.form['target_username']
+        username = session['username']
+        user_exist = search_user(target_username)
+        if user_exist:
+            inst = "INSERT INTO member VALUES (%s, %s, %s)"
+            cursor = conn.cursor()
+            cursor.execute(inst, (target_username, group_name, username))
+            cursor.commit()
+            cursor.close()
+            return redirect(url_for('friend_group'))
+        else:
+            ##need to implement for friend not exist
+            return redirect(url_for('friend_group'))
+    return render_template('add_friend.html')
+    
 @app.route('/post', methods=['GET', 'POST'])
+@login_required
 def post():
+       
 	username = session['username']
 	cursor = conn.cursor();
 	content = request.form['content']
@@ -107,15 +217,80 @@ def post():
 	cursor.close()
 	return redirect(url_for('home'))
 
+@app.route('/view/<int:item_id>/', methods=['GET'])
+@login_required
+def view(item_id):
+        inst = "SELECT * FROM content WHERE content.id = %s"
+        cursor = conn.cursor();
+        cursor.execute(inst, (item_id))
+        data = cursor.fetchall()
+        cursor.close()
+        return render_template('view.html', view_item = data)
+
+@app.route('/comment/<int:item_id>', methods=['GET', 'POST'])
+@login_required
+def comment(item_id):
+    return render_template('comment.html', content_id = item_id)
+
+@app.route('/post_comment', methods=['POST'])
+def post_comment():
+    if request.method == 'POST':
+        #data
+        content_id = request.form['content_id']
+        comment = request.form['comment']
+        username = session['username']
+        #time stamp
+        time = str(datetime.now())
+
+        inst = "INSERT INTO comment(id, username, timest, comment_text) VALUES (%s, %s, %s, %s)" 
+        cursor = conn.cursor();
+        cursor.execute(inst, (content_id, username, time, comment))
+        conn.commit()
+        cursor.close()
+        return redirect(url_for('home'))
+
 @app.route('/logout')
+@login_required
 def logout():
 	session.pop('username')
+	session.clear()
 	return redirect('/')
+
+@app.route('/profile') #MUST IMPLEMENT NOT MY profiles
+@login_required
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/myProfile')
+def myProfile():
+	username = session['username']
+	cur = conn.cursor();
+	friendsQuery = 'SELECT m.username, p.first_name, p.last_name FROM Member AS m\
+									JOIN Person AS p ON (m.username = p.username)\
+									WHERE (group_name,username_creator) IN (SELECT group_name,username_creator\
+											                                     FROM Member\
+											                                     WHERE username = %s)'
+	cur.execute(friendsQuery, (username))
+	friendsList = cur.fetchall()
+	cur.close()
+	return render_template('profile.html', user=username,friends=friendsList)
+
+
+@app.route('/notif') #MUST IMPLEMENT
+@login_required
+def notif():
+    return render_template('notif.html')
+
+
+@app.route('/home_nav') #MAY NEED TO IMPLEMENT, NOT SURE
+def home_nav():
+    return redirect(url_for('home'))
+
 
 app.secret_key = 'some key that you will never guess'
 
 if __name__ == "__main__":
-		app.run()
-
+    app.run(debug=True)
 
 
---
+++ email_module.py
@@ -0,0 +1,102 @@
+from socket import *
+import ssl #for SSL connection required by Gmail
+
+def mailPassword(new_pw, email):
+        msg = '\r\n Your temporary password is: %s!' % new_pw
+        endmsg ='\r\n.\r\n'
+        # Choose a mail server (e.g. Google mail server) and call it mailserver
+        mailServer = 'smtp.gmail.com'
+        port = 587
+        ##user credential for gmail
+        user = 'bWFkY2hhdHRlcjIwMTdAZ21haWwuY29t' #email = madchatter2017@gmail.com
+        password = 'cmJyYW96dGNlY3Bta210bQ==' 
+        ##connection
+        mySocket = socket(AF_INET, SOCK_STREAM)
+        mySocket.connect((mailServer, port))
+        recv = mySocket.recv(1024).decode('utf-8')
+        print(recv)
+        if recv[:3] != '220':
+            print ('220 reply not received from server.\n')
+        else:
+            print ('Initial connection established\n')
+            
+
+        # Send EHLO command and print server response.
+        print('Sending EHLO command to server.\n')
+        heloCommand = 'EHLO smtp.google.com\r\n'
+        mySocket.send(bytes(heloCommand, 'utf-8'))
+        recv1 = mySocket.recv(1024).decode('utf-8')
+        print(recv1)
+        if recv1[:3] != '250':
+            print ('250 reply not received from server.\n')
+        else:
+            print ('Server responded to EHLO\n')
+
+        # Request TLS
+        print('Starting TLS connection\n')
+        mySocket.sendall(bytes('STARTTLS\r\n','utf-8'))
+        tlsRecv = mySocket.recv(1024).decode('utf-8')
+        print(tlsRecv)
+        if tlsRecv[:3] != '220':
+            print ('220 reply not received from server\n')
+        else:
+            print ('TLS connection established\n')
+        # SSL
+        mySSL = ssl.wrap_socket(mySocket, ssl_version=ssl.PROTOCOL_SSLv23)
+        # Authorization
+        print('Begin authorization.\n')
+        authCommand = ('AUTH LOGIN %s\r\n' % user)
+        mySSL.sendall(bytes(authCommand, 'utf-8'))
+        authRecv = mySSL.recv(1024).decode('utf-8')
+        print(authRecv)
+        if authRecv[:3] != '334':
+            print('334 reply not received from server.\n')
+        else:
+            print('Server accepts authorization request.\n')
+        mySSL.sendall(bytes('%s\r\n' % password, 'utf-8'))
+        authRecv2 = mySSL.recv(1024).decode('utf-8')
+        print(authRecv2)
+        if authRecv2[:3] != '235':
+            print('235 reply not received from server.\n')
+        else:
+            print('Password accepted\n')
+            
+
+        # Send MAIL FROM command and print server response.
+        mySSL.sendall(bytes('MAIL FROM:<madchatter2017@gmail.com>\r\n', 'utf-8'))
+        recv2 = mySSL.recv(1024).decode('utf-8')
+        print(recv2) ##printing response
+        if recv2[:3] != '250':
+            print ('250 reply not received from server.\n')
+         
+        # Send RCPT TO command and print server response.
+        mySSL.sendall(bytes('RCPT TO:<%s>\r\n' % email, 'utf-8'))
+        recv3 = mySSL.recv(1024).decode('utf-8')
+        print(recv3) ##printing response
+        if recv3[:3] != '250':
+            print ('250 reply not received from server.\n')
+         
+
+        # Send DATA command and print server response.
+        mySSL.sendall(bytes('DATA\r\n', 'utf-8'))
+        recv4 = mySSL.recv(1024).decode('utf-8')
+        print(recv4) ##printing response
+        if recv4[:3] != '250':
+         print('250 reply not received from server.\n')
+         
+
+        # Send message data + ending message
+        mySSL.sendall(bytes(msg + '\r\n.\r\n', 'utf-8'))
+        recv5 = mySSL.recv(1024).decode('utf-8')
+        print(recv5) ##printing response
+        if recv5[:3] != '250':
+            print ('250 reply not received from server.')
+            
+
+        # Send QUIT command and get server response.
+        mySSL.sendall(bytes('QUIT\r\n', 'utf-8'))
+        recv6 = mySSL.recv(1024).decode('utf-8')
+        print(recv6) ##printing response
+        if recv6[:3] != '250':
+            print('250 reply not received from server.')
+
